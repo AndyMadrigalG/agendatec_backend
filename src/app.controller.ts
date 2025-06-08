0b6f3810c9cb3a71f9253b59cc0b0180d0a608c5
Andy Madrigal Garro <andymadrigal@estudiantec.cr>
2025-06-08 00:36:36 -0600

    swagger addition for documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpStatus} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Ejemplo de endpoint Hello World'
+  })
   hello(): string {
     return this.appService.getHello();
   }
 
   @Get('/is_api_up')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Valida que la API está funcionando'
+  })
   is_api_up(): string {
     return this.appService.is_api_up();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+   // Configuracion del documento Swagger para documentación de la API
+  const config = new DocumentBuilder()
+      .setTitle('Agendatec API')
+      .setDescription(
+          'Esta es la documentacion del API para la solucion para agendar sesiones de junta AgendaTec',
+      )
+      .setDescription('0.1.1')
+      .addTag('AgendaTec')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // Configura el endpoint de Swagger bajo la ruta /api_swagger
+  SwaggerModule.setup('api_swagger', app, document);
+
+  await app.listen(process.env.PORT ?? 3000); //prod env default port 3000 - local testing port 3001 para evitar conflictos de puerto
 }
 bootstrap();
