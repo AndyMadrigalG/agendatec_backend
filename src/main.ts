1c7dd448ee6a460f4e82e2ce25969efc93f8ecce
Andy Madrigal Garro <andymadrigal@estudiantec.cr>
2025-06-11 22:57:28 -0600

    trying to run container with secrets

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,6 @@ async function bootstrap() {
   // Configura el endpoint de Swagger bajo la ruta /api_swagger
   SwaggerModule.setup('api_swagger', app, document);
 
-  let current_directory = process.cwd();
-  console.log(">>>>>> THIS IS THE CURRENT DIRECTORY: "+' '+current_directory);
   // firebase auth initialization
   // const firebaseKeyFilePath = './firebase_service_account.json';
   // const firebaseServiceAccount /*: ServiceAccount*/ = JSON.parse(
