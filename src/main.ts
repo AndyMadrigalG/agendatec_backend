12e5afa7d6318b95eae2ed60672103a4b9804512
Andy Madrigal Garro <64030322+AndyMadrigalG@users.noreply.github.com>
2025-06-12 10:23:03 -0600

    Fix/secret variables (#36)
    * update path
    
    * failing because cannot find secret json
    
    * if no json then dont set firebase but dont FAIL
    
    * last changes to find json secrets
    
    * path changes depending on OS
    
    * last change to find firebase json path

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   // firebase auth initialization
   let firebaseFilePath = os.platform() === 'win32'
       ? path.join(__dirname, '..', 'firebase_service_account.json')
-      : path.join(__dirname, '..', '..', '..', 'firebase_service_account.json');
+      : path.join(__dirname, '..', '..', '..', '..', 'firebase_service_account.json');
 
   let firebaseServiceAccount;
   if (!fs.existsSync(firebaseFilePath)) {
