327662bdfb3b5ef372264a78cb37a12d0953bd50
Sully-2403 <100237035+Sully-2403@users.noreply.github.com>
2025-06-15 03:22:08 -0600

    Feature/Junta (#97)
    * junta features
    
    * Update app.module.ts

---
+++ src/junta/junta.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { JuntaService } from './junta.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { JuntaDto } from './dto/junta.dto';
+
+@Controller('junta')
+export class JuntaController {
+    constructor(private juntaService: JuntaService) {}
+
+    @Post()
+    @UsePipes(new ValidationPipe({ transform: true }))
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'Registra una nueva junta',
+    })
+    postJunta(@Body() juntaDto: JuntaDto) {
+        return this.juntaService.postJunta(juntaDto);
+    }
+
+    @Get()
+        @ApiResponse({ status: 200, description: 'Lista de juntas' })
+        getJuntas() {
+            return this.juntaService.getJuntas();
+        }
+}
