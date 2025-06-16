639d1f1643faf70ba313e8373eb54590af9b46a7
Andy Madrigal Garro <andymadrigal@estudiantec.cr>
2025-06-16 00:17:41 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ import * as firebaseAdmin from 'firebase-admin';
 import * as fs from 'fs';
 import * as path from "node:path";
 
-const PROD_URL = 'https://agendatec-frontend-371160271556.us-central1.run.app'
-const DEV_URL = 'http://localhost:3001'
+const DEV_URL = process.env.DEV_FRONTEND_URL || 'http://localhost:3001'
+const PROD_URL = process.env.PROD_URL || 'https://agendatec-frontend-371160271556.us-central1.run.app'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [PROD_URL, DEV_URL], // Configura el origen permitido
+    origin: [DEV_URL,PROD_URL], // Configura el origen permitido
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
     credentials: true, // Permite el envío de cookies y encabezados de autorización
   });
@@ -24,7 +24,7 @@ async function bootstrap() {
       .setDescription(
           'Esta es la documentacion del API para la solucion para agendar sesiones de junta AgendaTec',
       )
-      .setDescription('0.1.1')
+      .setDescription('0.1.2')
       .addTag('AgendaTec')
       .build();
   const document = SwaggerModule.createDocument(app, config);
@@ -57,6 +57,6 @@ async function bootstrap() {
   }
   console.log("la database url: "+process.env.DATABASE_URL);
   console.log(" la firebase api: "+process.env.FIREBASE_API_KEY);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.BACKEND_PORT ?? 3000);
 }
 bootstrap();
