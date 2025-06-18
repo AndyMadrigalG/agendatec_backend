d4822ff53a6b7594743255faaa014327da09f809
Andy Madrigal Garro <andymadrigal@estudiantec.cr>
2025-06-18 13:14:02 -0600

    rollback to valid firebase auth state

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as firebaseAdmin from 'firebase-admin';
+import * as fs from 'fs';
+import * as path from "node:path";
 
 const DEV_URL = process.env.DEV_FRONTEND_URL || 'http://localhost:3001'
 const PROD_URL = process.env.PROD_URL || 'https://agendatec-frontend-371160271556.us-central1.run.app'
@@ -28,8 +31,30 @@ async function bootstrap() {
   // Configura el endpoint de Swagger bajo la ruta /api_swagger
   SwaggerModule.setup('api_swagger', app, document);
 
+  // firebase auth initialization
+  let firebaseServiceAccount: firebaseAdmin.ServiceAccount | undefined = undefined;
+  const firebaseFilePath : string = path.join(__dirname, '..', 'firebase_service_account.json');
 
+  if (!fs.existsSync(firebaseFilePath)) {
+    console.log('Firebase service account key file not found at:', firebaseFilePath);
+  } else {
+    console.log('Firebase service account key file found at:', firebaseFilePath);
+    const raw = fs.readFileSync(firebaseFilePath, 'utf8');
 
+    try {
+      firebaseServiceAccount = JSON.parse(raw);
+    } catch (error) {
+      console.error('Error parsing Firebase service account JSON:', error);
+    }
+
+  }
+
+  if (firebaseAdmin.apps.length === 0 && firebaseServiceAccount !== undefined) {
+    firebaseAdmin.initializeApp({
+      credential: firebaseAdmin.credential.cert(firebaseServiceAccount),
+    });
+    console.log('Initialized Firebase Auth');
+  }
   console.log("la database url: "+process.env.DATABASE_URL);
   console.log(" la firebase api: "+process.env.FIREBASE_API_KEY);
   await app.listen(process.env.BACKEND_PORT ?? 8080); // in prod needs to be 8080
