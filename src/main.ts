6efeec2af85c9de4e57edb558d02db20361ef806
Andy Madrigal Garro <andymadrigal@estudiantec.cr>
2025-06-17 20:36:05 -0600

    comment

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,6 @@ async function bootstrap() {
   }
   console.log("la database url: "+process.env.DATABASE_URL);
   console.log(" la firebase api: "+process.env.FIREBASE_API_KEY);
-  await app.listen(process.env.BACKEND_PORT ?? 8080);
+  await app.listen(process.env.BACKEND_PORT ?? 8080); // in prod needs to be 8080
 }
 bootstrap();
